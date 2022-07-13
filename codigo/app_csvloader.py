51ca3e15a41b49f7185c128dc1477383436fa668
wasf84 <wfilho@ice.ufjf.br>
2022-07-13 13:12:17 -0300

    trabalhando ainda no metodo de carregar arquivo CSV

--- codigo/app_csvloader.py
+++ codigo/app_csvloader.py
@@ -2,7 +2,7 @@
 
 import os, csv, pandas
 
-from PyQt5.QtWidgets import QFileDialog, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QMessageBox, QTableWidgetItem
 from main_csvloader import Ui_wndwCSVLoader
 
 class App_csvloader(QMainWindow, Ui_wndwCSVLoader):
@@ -11,6 +11,8 @@ class App_csvloader(QMainWindow, Ui_wndwCSVLoader):
         self.setupUi(self)
         self.connectSignalsSlots()
 
+        self.hasHeaders = False
+
     def connectSignalsSlots(self):
         self.actionSair.triggered.connect(self.close)
         self.actionAbrir_arquivo.triggered.connect(self.abrirArquivo)
@@ -18,46 +20,51 @@ class App_csvloader(QMainWindow, Ui_wndwCSVLoader):
     def abrirArquivo(self):
         arq = QFileDialog.getOpenFileName(self, 'Abrir arquivo CSV', '', "Arquivos CSV (*.csv)")
         
-        if arq:
-            df = pandas.read_csv(arq, header=None, delimiter=',', keep_default_na=False, error_bad_lines=False)
-            header = df.iloc[0]
+        #with arq:
+        df = pandas.read_csv(arq, header=None, delimiter='\t', keep_default_na=False, on_bad_lines='warn')
+            #header = df.iloc[0]
+            
+            #print(df)
 
-        ### o arquivo tem a primeira linha como cabeçalho?
-            ret = QMessageBox.question(self, "CSV Loader", "usar a 1a linha de cabeçalho?\n\n" + str(header.values),
-                    QMessageBox.Ok | QMessageBox.No, defaultButton = QMessageBox.Ok)
-            if ret == QMessageBox.Ok:
-                df = df[1:]
-
-                self.tblDados.setColumnCount(len(df.columns))
-                self.tblDados.setRowCount(len(df.index))
-        
-                for i in range(len(df.index)):
-                    for j in range(len(df.columns)):
-                        self.tblDados.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
-    
-                for j in range(len(df.columns)):
-                    m = QTableWidgetItem(header[j])
-                    self.tblDados.setHorizontalHeaderItem(j, m)
-                    
-                self.hasHeaders = True
-
-            else:
-                self.tblDados.setColumnCount(len(df.columns))
-                self.tblDados.setRowCount(len(df.index))
-        
-                for i in range(len(df.index)):
-                    for j in range(len(df.columns)):
-                        self.tblDados.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
-
-                for j in range(self.tblDados.columnCount()):
-                    m = QTableWidgetItem(str(j))
-                    self.tblDados.setHorizontalHeaderItem(j, m)
-                    
-                self.hasHeaders = False
-    
-            self.tblDados.selectRow(0)
-            self.isChanged = False
-            self.setCurrentFile(fileName)
-            self.tblDados.resizeColumnsToContents()
-            self.tblDados.resizeRowsToContents()
-            self.msg(fileName + " loaded")
+        #--------------------------------------------------------------- if arq:
+            # df = pandas.read_csv(arq, header=None, delimiter=',', keep_default_na=False, on_bad_lines='skip')
+            #----------------------------------------------- header = df.iloc[0]
+#------------------------------------------------------------------------------ 
+        #-------------------- ### o arquivo tem a primeira linha como cabeçalho?
+            #---------------------------------- ret = QMessageBox.question(self,
+                                       # "CSV Loader", "usar a 1a linha de cabeçalho?\n\n" + str(header.values),
+                                       #------- QMessageBox.Ok | QMessageBox.No,
+                                       #-------- defaultButton = QMessageBox.Ok)
+            #----------------------------------------- if ret == QMessageBox.Ok:
+                #--------------------------------------------------- df = df[1:]
+#------------------------------------------------------------------------------ 
+                #----------------- self.tblDados.setColumnCount(len(df.columns))
+                #---------------------- self.tblDados.setRowCount(len(df.index))
+#------------------------------------------------------------------------------ 
+                #-------------------------------- for i in range(len(df.index)):
+                    #-------------------------- for j in range(len(df.columns)):
+                        # self.tblDados.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
+#------------------------------------------------------------------------------ 
+                #------------------------------ for j in range(len(df.columns)):
+                    #--------------------------- m = QTableWidgetItem(header[j])
+                    #--------------- self.tblDados.setHorizontalHeaderItem(j, m)
+#------------------------------------------------------------------------------ 
+                #---------------------------------------- self.hasHeaders = True
+#------------------------------------------------------------------------------ 
+            #------------------------------------------------------------- else:
+                #----------------- self.tblDados.setColumnCount(len(df.columns))
+                #---------------------- self.tblDados.setRowCount(len(df.index))
+#------------------------------------------------------------------------------ 
+                #-------------------------------- for i in range(len(df.index)):
+                    #-------------------------- for j in range(len(df.columns)):
+                        # self.tblDados.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
+#------------------------------------------------------------------------------ 
+                #------------------ for j in range(self.tblDados.columnCount()):
+                    #------------------------------ m = QTableWidgetItem(str(j))
+                    #--------------- self.tblDados.setHorizontalHeaderItem(j, m)
+#------------------------------------------------------------------------------ 
+                #--------------------------------------- self.hasHeaders = False
+#------------------------------------------------------------------------------ 
+            #---------------------------------------- self.tblDados.selectRow(0)
+            #--------------------------- self.tblDados.resizeColumnsToContents()
+            #------------------------------ self.tblDados.resizeRowsToContents()
